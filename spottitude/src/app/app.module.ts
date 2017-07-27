0656c3189f0858f976905bd9149ebd430dca1053
Dave Colgan <dcolgan88@gmail.com>
2017-07-27 12:50:36 +0100

    Component route redo

--- spottitude/src/app/app.module.ts
+++ spottitude/src/app/app.module.ts
@@ -4,16 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { SearchComponent } from './search/search.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    SearchComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
